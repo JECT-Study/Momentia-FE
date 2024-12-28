c63661dd103e58dc238c7c559c1606bce78786ac
Dahyeon Jin <wlsekgus12@gmail.com>
2024-12-29 06:28:33 +0900

    style: Search Bar UI 추가

--- src/app/artwork/list/page.tsx
+++ src/app/artwork/list/page.tsx
@@ -60,7 +60,7 @@ const ArtworkList = () => {
         </button>
 
         {showFollowedArtistsCards && (
-          <div className='pr-[31px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[30px]'>
+          <div className='pr-[31px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[30px] pb-[130px]'>
             {followedArtistsData.map((artist: any) => (
               <div
                 key={artist.userId}
@@ -110,6 +110,20 @@ const ArtworkList = () => {
             )}
           </div>
         )}
+
+        <div className='flex items-center justify-center'>
+          <div className='flex items-center w-[760px] h-[78px] px-5 rounded-[10px] bg-gray-900 mb-[110px]'>
+            <input
+              className='flex-grow body1 bg-transparent border-none focus:outline-none placeholder-gray-500 focus:text-white'
+              placeholder={`'작품 제목 또는 작가 이름'으로 검색`}
+            />
+            <Icon
+              name='Search'
+              size='l'
+              className='text-white flex-shrink-0 ml-3'
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
