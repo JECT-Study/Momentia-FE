533408fb2d8965a9bb29a839c41d2c05d3756087
Dahyeon Jin <wlsekgus12@gmail.com>
2025-01-01 16:09:06 +0900

    feat: 에러 처리를 위한 try-catch 블록 추가

--- src/hooks/useFollowedArtists.ts
+++ src/hooks/useFollowedArtists.ts
@@ -6,8 +6,13 @@ import QUERY_KEYS from '@/constants/queryKeys';
 import { useQuery } from '@tanstack/react-query';
 
 export const getFollowedArtists = async () => {
-  const response = await defaultClient.get(ARTWORK.followedArtists);
-  return response.data.posts;
+  try {
+    const response = await defaultClient.get(ARTWORK.followedArtists);
+    return response.data.posts;
+  } catch (error) {
+    console.error('내가 팔로우한 작가 조회 중 에러 발생: ', error);
+    throw new Error('내가 팔로우한 작가 조회 실패');
+  }
 };
 
 export const useFollowedArtists = () => {
