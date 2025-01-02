4d184f67e4e6abc6f15dafcdb736ab88425d078c
Dahyeon Jin <wlsekgus12@gmail.com>
2025-01-03 02:01:59 +0900

    refactor: FollowButtonProps에 필요 없는 variant prop 제거

--- src/types/buttons/FollowButtonProps.ts
+++ src/types/buttons/FollowButtonProps.ts
@@ -3,7 +3,13 @@ import { BaseButtonProps } from './BaseButtonProps';
 export interface FollowButtonProps
   extends Omit<
     BaseButtonProps,
-    'children' | 'disabled' | 'loading' | 'icon' | 'iconPosition' | 'type'
+    | 'variant'
+    | 'children'
+    | 'disabled'
+    | 'loading'
+    | 'icon'
+    | 'iconPosition'
+    | 'type'
   > {
   initFollowState: boolean;
   followUserId: number;
