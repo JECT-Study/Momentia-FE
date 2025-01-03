116b8e8f44bea00a00bd602bbe7961d0d3309265
Parksangwoo <psu8430@gmail.com>
2025-01-04 05:02:41 +0900

    feat: token 제거 로직 추가

--- src/utils/tokenHandler.ts
+++ src/utils/tokenHandler.ts
@@ -8,12 +8,19 @@ const TokenHandler = {
   }) {
     localStorage.setItem('accessToken', accessToken);
     sessionStorage.setItem('refreshToken', refreshToken);
+    window.dispatchEvent(new Event('authEvent'));
   },
   getAccessToken() {
     return localStorage.getItem('accessToken') || '';
   },
   getRefreshToken() {
     return sessionStorage.getItem('refreshToken') || '';
+  },
+
+  removeToken() {
+    localStorage.removeItem('accessToken');
+    sessionStorage.removeItem('refreshToken');
+    window.dispatchEvent(new Event('authEvent'));
   },
 };
 
