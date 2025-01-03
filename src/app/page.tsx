08591752b20e1e8caee17bfbb2afd8f14c909f31
Dahyeon Jin <wlsekgus12@gmail.com>
2025-01-04 06:40:46 +0900

    chore: 주석 해제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import LatestArtworkSection from '@/components/MainPage/LatestArtworkSection';
 import MonthlyBestArtistSection from '@/components/MainPage/MonthlyBestArtistSection';
 import MonthlyPopularArtworkSection from '@/components/MainPage/MonthlyPopularArtworkSection';
 
@@ -7,7 +8,7 @@ const Home = () => {
   return (
     <div className='max-w-[1960px] w-full mx-auto my-[126px] flex flex-col gap-[126px] px-[32px] tablet:px-[140px]'>
       <MonthlyPopularArtworkSection />
-      {/* <LatestArtworkSection /> */}
+      <LatestArtworkSection />
       <MonthlyBestArtistSection />
     </div>
   );
