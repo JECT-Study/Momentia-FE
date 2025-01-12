9f2eb3d2ef80cbe2bc70d16a38d420bb407d2625
Dahyeon Jin <wlsekgus12@gmail.com>
2025-01-12 13:12:47 +0900

    fix: accessToken이 없는 경우, API 요청 차단

--- src/hooks/serverStateHooks/useFollowedArtists.ts
+++ src/hooks/serverStateHooks/useFollowedArtists.ts
@@ -2,11 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 
 import getFollowedArtists from '@/apis/artwork/getFollowedArtists';
 import { ARTWORK } from '@/constants/API';
+import TokenHandler from '@/utils/tokenHandler';
 
 const useFollowedArtists = () => {
+  const accessToken = TokenHandler.getAccessToken();
+
   return useQuery({
     queryKey: [ARTWORK.followedArtists],
     queryFn: getFollowedArtists,
+    enabled: !!accessToken,
   });
 };
 
