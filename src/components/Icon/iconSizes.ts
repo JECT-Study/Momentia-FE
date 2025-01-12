64e476872ef6189f274dd75f873013e822726440
Dahyeon Jin <wlsekgus12@gmail.com>
2025-01-12 13:51:11 +0900

    remove: 글로벌로 지정된 spacing 제거

--- src/components/Icon/iconSizes.ts
+++ src/components/Icon/iconSizes.ts
@@ -1,10 +1,10 @@
 export type IconSize = 'xl' | 'l' | 'm' | 's';
 
 const iconSizes: Record<IconSize, string> = {
-  xl: 'w-15 h-15',
-  l: 'w-7.5 h-7.5',
+  xl: 'w-[60px] h-[60px]',
+  l: 'w-[30px] h-[30px]',
   m: 'w-6 h-6',
-  s: 'w-4.5 h-4.5',
+  s: 'w-[18px] h-[18px]',
 };
 
 export default iconSizes;
