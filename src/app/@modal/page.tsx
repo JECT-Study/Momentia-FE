aaba86209d6c749050b83f53db1209325a2f7f81
Parksangwoo <psu8430@gmail.com>
2025-01-15 16:57:11 +0900

    style: 색상 프리셋 적용

--- src/app/@modal/page.tsx
+++ src/app/@modal/page.tsx
@@ -28,7 +28,7 @@ const ModalPage = () => {
       {isOpen && (
         <div className='fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50'>
           <div
-            className={`${modalClassNames[modalSize]} absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[366px] h-content bg-[#232225] text-black`}
+            className={`${modalClassNames[modalSize]} absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[366px] h-content bg-background-overlay text-black`}
           >
             {contents}
           </div>
