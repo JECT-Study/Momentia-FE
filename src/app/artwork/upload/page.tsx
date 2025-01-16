2722009cc6e171e3e9ad7b88ae6b5d9b79b934fe
Dahyeon Jin <wlsekgus12@gmail.com>
2025-01-17 07:12:36 +0900

    feat: 필수 항목의 유효성 검사 추가

--- src/app/artwork/upload/page.tsx
+++ src/app/artwork/upload/page.tsx
@@ -1,28 +1,26 @@
 'use client';
 
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 import OvalButton from '@/components/Button/OvalButton';
 import FilterDropdown from '@/components/FilterDropdown';
 import Icon from '@/components/Icon/Icon';
 import BasicInput from '@/components/Input/BasicInput';
 
+interface ArtworkField {
+  name: string;
+  value: string;
+}
+
 const MAX_TITLE_LENGTH = 50;
 const MAX_DESCRIPTION_LENGTH = 1000;
 
 const PRIVACY_SETTING_OPTIONS = [
-  {
-    name: '전체공개',
-    value: 'PUBLIC',
-  },
-  {
-    name: '비공개',
-    value: 'PRIVATE',
-  },
+  { name: '전체공개', value: 'PUBLIC' },
+  { name: '비공개', value: 'PRIVATE' },
 ];
 
-const ARTWORK_FIELDS = [
-  { name: '전체', value: 'ALL' },
+const ARTWORK_FIELDS: ArtworkField[] = [
   { name: '회화', value: 'PAINTING' },
   { name: '공예/조각', value: 'CRAFTSCULPTURE' },
   { name: '드로잉', value: 'DRAWING' },
@@ -34,47 +32,105 @@ const ARTWORK_FIELDS = [
   { name: '기타', value: 'OTHERS' },
 ];
 
-interface ArtworkFilterProps {
-  selectedArtworkField: string;
-  setSelectedArtworkField: (value: string | ((prev: string) => string)) => void;
-  selectedFilter: string;
-  setSelectedFilter: (value: string | ((prev: string) => string)) => void;
-  setCurrentPage: (value: number | ((prev: number) => number)) => void;
-}
-
-interface ArtworkField {
-  name: string;
-  value: string;
-}
-
 const ArtworkUpload = () => {
   const [artworkTitle, setArtworkTitle] = useState('');
   const [selectedArtworkField, setSelectedArtworkField] = useState('');
-  const [privacySetting, setPrivacySetting] = useState<'public' | 'private'>(
-    'public',
+  const [privacySetting, setPrivacySetting] = useState<'전체공개' | '비공개'>(
+    '전체공개',
   );
+  const [uploadedImage, setUploadedImage] = useState<string | null>(null);
   const [artworkDescription, setArtworkDescription] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errors, setErrors] = useState<{
+    artworkTitle?: string;
+    selectedArtworkField?: string;
+    uploadedImage?: string;
+  }>({
+    artworkTitle: '',
+    selectedArtworkField: '',
+    uploadedImage: '',
+  });
 
-  const handleArtworkTitleOnChange = (e: any) => {
+  const handleArtworkTitleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
     setArtworkTitle(e.target.value);
+
+    if (errors.artworkTitle) {
+      setErrors((prevErrors) => ({ ...prevErrors, artworkTitle: '' }));
+    }
   };
 
-  const selectedArtworkFieldName =
-    ARTWORK_FIELDS.find((field) => field.value === selectedArtworkField)
-      ?.name || '전체';
+  const handlertworkDescriptionOnChange = (
+    e: ChangeEvent<HTMLInputElement>,
+  ) => {
+    setArtworkDescription(e.target.value);
+  };
 
   const handleArtworkFieldClick = (artworkField: string) => {
     setSelectedArtworkField(artworkField);
+
+    if (!artworkTitle.trim()) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        artworkTitle: '필수 항목입니다.',
+      }));
+    } else {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        artworkTitle: '',
+      }));
+    }
+
+    if (errors.selectedArtworkField) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        selectedArtworkField: '',
+      }));
+    }
   };
 
   const handlePrivacySettingChange = (
-    newPrivacySetting: 'public' | 'private',
+    newPrivacySetting: '전체공개' | '비공개',
   ) => {
     setPrivacySetting(newPrivacySetting);
   };
 
-  const handlertworkDescriptionOnChange = (e: any) => {
-    setArtworkDescription(e.target.value);
+  const handleImageUpload = () => {
+    // TODO: 실제 이미지 업로드 로직 구현
+    setUploadedImage('uploaded-image-url'); // 테스트용
+
+    if (errors.uploadedImage) {
+      setErrors((prevErrors) => ({ ...prevErrors, uploadedImage: '' }));
+    }
+  };
+
+  const validateArtworkUploadForm = () => {
+    const newErrors: {
+      artworkTitle?: string;
+      selectedArtworkField?: string;
+      uploadedImage?: string;
+    } = {};
+
+    if (!artworkTitle.trim()) newErrors.artworkTitle = '필수 항목입니다.';
+    if (!selectedArtworkField.trim())
+      newErrors.selectedArtworkField = '필수 항목입니다.';
+    if (!uploadedImage) newErrors.uploadedImage = '이미지를 업로드해주세요.';
+    setErrors(newErrors);
+
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const isFormValid =
+    artworkTitle && selectedArtworkField && uploadedImage && !isSubmitting;
+
+  const handleArtworkUpload = () => {
+    if (!validateArtworkUploadForm()) return;
+    setIsSubmitting(true);
+
+    // TODO: 업로드 성공 시, 작성한 글 상세 페이지로 이동
+    // [테스트용] 업로드 처리 (API 호출)
+    setTimeout(() => {
+      console.log('업로드 완료');
+    }, 2000);
   };
 
   return (
@@ -89,22 +145,25 @@ const ArtworkUpload = () => {
           onChange={handleArtworkTitleOnChange}
           showTextLength={true}
           maxLength={MAX_TITLE_LENGTH}
-          // isInvalid={!!errors.artworkTitle}
-          // errorMessage={errors.artworkTitle?.message as string}
+          isInvalid={!!errors.artworkTitle}
+          errorMessage={errors.artworkTitle}
         />
       </div>
       <div className='flex flex-col md:flex-row items-start self-stretch gap-[38px] md:gap-[50px] pb-[78px]'>
         <FilterDropdown
           label='작품 카테고리'
           placeholder='카테고리 선택'
-          options={ARTWORK_FIELDS}
+          options={ARTWORK_FIELDS.map((field) => field.name)}
           selected={selectedArtworkField}
-          onClick={() => handleArtworkFieldClick(artworkField.value)}
+          onChange={(value) => handleArtworkFieldClick(value)}
+          isInvalid={!!errors.selectedArtworkField}
+          errorMessage={errors.selectedArtworkField}
           className='w-full'
         />
+
         <FilterDropdown
           label='공개범위'
-          options={PRIVACY_SETTING_OPTIONS}
+          options={PRIVACY_SETTING_OPTIONS.map((option) => option.name)}
           selected={privacySetting}
           onChange={() => handlePrivacySettingChange(privacySetting)}
           className='w-full'
@@ -125,13 +184,38 @@ const ArtworkUpload = () => {
             <br />
             작품 업로드 버튼을 눌러 이미지를 선택하세요.
           </p>
-          <OvalButton variant='primary' buttonSize='s'>
+          <OvalButton
+            variant='primary'
+            buttonSize='s'
+            onClick={handleImageUpload}
+          >
             <Icon name='UploadShare' size='m' className='mr-2.5' />
-            작품 업로드
+            이미지 업로드
           </OvalButton>
           <p className='button-s text-center text-gray-500'>
             작품 이미지는 1장만 업로드 가능합니다.
           </p>
+
+          {errors.uploadedImage && (
+            <div className='flex items-center mt-[3px] h-[26px]'>
+              <Icon
+                name='AlertCircle'
+                size='s'
+                className='text-system-error mr-2'
+              />
+              <p className='button-s text-system-error'>
+                {errors.uploadedImage}
+              </p>
+            </div>
+          )}
+
+          {uploadedImage && (
+            <img
+              src={uploadedImage}
+              alt='Uploaded Artwork'
+              className='mt-4 max-h-[200px] object-contain'
+            />
+          )}
 
           <button
             aria-label='Button to change artwork image'
@@ -163,7 +247,14 @@ const ArtworkUpload = () => {
         >
           취소
         </button>
-        <OvalButton variant='primary' buttonSize='s'>
+
+        {/* hover:cursor-not-allowed */}
+        <OvalButton
+          variant={`${!isSubmitting && isFormValid ? 'primary' : 'secondary'}`}
+          buttonSize='s'
+          disabled={isSubmitting && !isFormValid}
+          onClick={handleArtworkUpload}
+        >
           업로드
         </OvalButton>
       </div>
