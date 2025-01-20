f9d36349c7fa8a087d64f6b550ab120c3e23ca85
Dahyeon Jin <wlsekgus12@gmail.com>
2025-01-20 11:16:06 +0900

    feat: 작품 업로드 POST API 추가

---
+++ src/apis/artwork/postArtwork.ts
@@ -0,0 +1,25 @@
+import defaultClient from '@/apis';
+import { ARTWORK } from '@/constants/API';
+
+interface uploadArtworkData {
+  title: string;
+  artworkField: string;
+  postImage: string;
+  explanation: string;
+  status: string;
+}
+
+const postArtwork = async (artworkData: uploadArtworkData) => {
+  try {
+    const response = await defaultClient.post(
+      ARTWORK.uploadArtwork,
+      artworkData,
+    );
+    return response.data;
+  } catch (error) {
+    console.error('작품 업로드 중 에러 발생: ', error);
+    throw new Error('작품 업로드에 실패하였습니다. 다시 시도해주세요.');
+  }
+};
+
+export default postArtwork;
