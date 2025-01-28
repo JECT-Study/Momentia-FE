6115cfa3afd4bb75ee03ca4997370d2853dfccf1
Parksangwoo <psu8430@gmail.com>
2025-01-28 23:12:24 +0900

    feat: 댓글 수정 컴포넌트 추가

---
+++ src/components/ArtworkDetailPage/ArtworkCommentSection/ArtworkCommentUnit/CommentEdit.tsx
@@ -0,0 +1,64 @@
+import { ChangeEvent, useState } from 'react';
+
+import OvalButton from '@/components/Button/OvalButton';
+import Textarea from '@/components/Input/Textarea';
+import usePatchArtworkComment from '@/hooks/serverStateHooks/usePatchArtworkComment';
+import { CommentControllerProps } from '@/types/comment';
+
+const CommentEdit = ({
+  comment,
+  postId,
+  setIsEditMode,
+}: CommentControllerProps) => {
+  const { content, commentId } = comment;
+
+  const { mutate: editComment } = usePatchArtworkComment(commentId, postId);
+
+  const [commentText, setCommentText] = useState(content);
+
+  const changeCommentText = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setCommentText(event.target.value);
+  };
+
+  const cancelEditMode = () => {
+    setIsEditMode(false);
+  };
+
+  const clickEditButton = () => {
+    editComment(commentText, {
+      onSuccess: () => {
+        setIsEditMode(false);
+
+        // 토스트 메세지 적용
+        alert('댓글 수정 성공');
+      },
+    });
+  };
+
+  return (
+    <>
+      <Textarea value={commentText} onChange={changeCommentText} />
+      <div className='flex justify-end gap-2.5'>
+        <OvalButton
+          disabled={commentText.length === 0}
+          buttonSize={'s'}
+          variant={'primary'}
+          className='!bg-gray-900'
+          onClick={cancelEditMode}
+        >
+          <p className='button-s'>취소</p>
+        </OvalButton>
+        <OvalButton
+          disabled={commentText.length === 0}
+          buttonSize={'s'}
+          variant={'primary'}
+          onClick={clickEditButton}
+        >
+          <p className='button-s'>완료</p>
+        </OvalButton>
+      </div>
+    </>
+  );
+};
+
+export default CommentEdit;
