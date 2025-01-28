22e55632798f550cf36253dc96868fe51bf0075c
Parksangwoo <psu8430@gmail.com>
2025-01-28 23:16:31 +0900

    feat: 상세 페이지 추가

---
+++ src/app/artwork/detail/page.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+import ArtistInfoSection from '@/components/ArtworkDetailPage/ArtistInfoSection';
+import ArtworkCommentSection from '@/components/ArtworkDetailPage/ArtworkCommentSection';
+import ArtworkDetailHeader from '@/components/ArtworkDetailPage/ArtworkDetailHeader';
+import ArtworkDetailInfoSection from '@/components/ArtworkDetailPage/ArtworkDetailInfoSection';
+import ButtonGroup from '@/components/ArtworkDetailPage/ButtonGroup';
+import useGetArtworkPost from '@/hooks/serverStateHooks/useGetArtworkPost';
+
+const ArtworkDetailPage = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const postId = Number(searchParams.get('postId'));
+
+  const { headerInfo, socialInfo, detailInfo, artistInfo, isLoading } =
+    useGetArtworkPost(postId);
+
+  useEffect(() => {
+    if (!postId) router.back();
+  }, [postId]);
+
+  if (isLoading || !headerInfo || !socialInfo || !detailInfo || !artistInfo)
+    return <div>Loading</div>;
+  return (
+    <div className='relative flex flex-col gap-[60px] w-full max-w-[1920px] m-auto py-[70px] tablet:px-[140px] px-[32px]'>
+      {/* 작품 영역 */}
+      <ArtworkDetailHeader headerInfo={headerInfo} />
+      <div className='relative flex flex-col tablet:gap-[40px] gap-[30px]'>
+        <ArtworkDetailInfoSection detailInfo={detailInfo} />
+        {/* 우측 버튼 그룹 */}
+        <ButtonGroup socialInfo={socialInfo} />
+        <div className='flex tablet:flex-row flex-col tablet:gap-10 gap-[70px] mt-10'>
+          {/* 작가 정보 영역 */}
+          <ArtistInfoSection artistInfo={artistInfo} />
+          {/* 댓글 영영 */}
+          <ArtworkCommentSection postId={postId} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ArtworkDetailPage;
