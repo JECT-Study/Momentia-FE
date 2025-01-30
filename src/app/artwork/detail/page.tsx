a55d9a79941c1c7d7ed76791d4898806bc89fce6
Parksangwoo <psu8430@gmail.com>
2025-01-31 01:22:39 +0900

    chore: 페이지 컴포넌트 내 주석 삭제

--- src/app/artwork/detail/page.tsx
+++ src/app/artwork/detail/page.tsx
@@ -26,16 +26,12 @@ const ArtworkDetailPage = () => {
     return <div>Loading</div>;
   return (
     <div className='relative flex flex-col gap-[60px] w-full max-w-[1920px] m-auto py-[70px] tablet:px-[140px] px-[32px]'>
-      {/* 작품 영역 */}
       <ArtworkDetailHeader headerInfo={headerInfo} />
       <div className='relative flex flex-col tablet:gap-[40px] gap-[30px]'>
         <ArtworkDetailInfoSection detailInfo={detailInfo} />
-        {/* 우측 버튼 그룹 */}
         <ButtonGroup socialInfo={socialInfo} />
         <div className='flex tablet:flex-row flex-col tablet:gap-10 gap-[70px] mt-10'>
-          {/* 작가 정보 영역 */}
           <ArtistInfoSection artistInfo={artistInfo} />
-          {/* 댓글 영영 */}
           <ArtworkCommentSection postId={postId} />
         </div>
       </div>
