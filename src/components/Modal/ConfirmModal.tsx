f1a36add6676fce487f0e6ed1590ccd30723733b
Parksangwoo <psu8430@gmail.com>
2025-02-03 04:14:34 +0900

    chore: 불필요함 함수 삭제

--- src/components/Modal/ConfirmModal.tsx
+++ src/components/Modal/ConfirmModal.tsx
@@ -26,11 +26,6 @@ const ConfirmModal = ({
 }: ConfirmModalProps) => {
   const { closeModal } = useStore(modalStore);
 
-  const handleConfirm = () => {
-    closeModal();
-    window.history.back();
-  };
-
   const buttonOrderClassName = isButtonOnRow
     ? reverseButtonOrder
       ? 'flex-row-reverse'
