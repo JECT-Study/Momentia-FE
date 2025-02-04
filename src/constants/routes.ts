062442b06b5cd549d168c99cdf0dc82191735ccb
Dahyeon Jin <wlsekgus12@gmail.com>
2025-02-05 01:25:32 +0900

    fix: 중복되는 ROUTE 삭제

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,7 +2,6 @@ const ROUTE = {
   home: '/',
   signIn: '/auth/sign-in',
   signUp: '/auth/sign-up',
-  artworkDetail: '/artwork/detail',
   artworkList: '/artwork/list',
   artworkDetail: '/artwork/detail',
   artworkUpload: '/artwork/upload',
