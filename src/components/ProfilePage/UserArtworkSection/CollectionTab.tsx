94090bc5dded80fbb7aa038bb63f1e4fa60302da
Parksangwoo <psu8430@gmail.com>
2025-02-09 02:15:51 +0900

    feat: 컬랙션 탭 내 컨텐츠가 비었을 경우에 대한 안내 메세지 추가

--- src/components/ProfilePage/UserArtworkSection/CollectionTab.tsx
+++ src/components/ProfilePage/UserArtworkSection/CollectionTab.tsx
@@ -1,5 +1,11 @@
 const CollectionTab = () => {
-  return <div>CollectionTab</div>;
+  return (
+    <div>
+      <div className='py-[620px] text-center'>
+        <p className='body1 text-gray-500'>아직 컬렉션이 비어있어요.</p>
+      </div>
+    </div>
+  );
 };
 
 export default CollectionTab;
