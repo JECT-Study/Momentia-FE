3b15b734c79fe76be89c07dc335d61e004f4621a
Dahyeon Jin <wlsekgus12@gmail.com>
2025-02-08 22:14:17 +0900

    feat: 프로필 페이지에서 컬렉션을 생성할 경우, 컬렉션 데이터를 UI에 즉시 업데이트

--- src/app/auth/profile/page.tsx
+++ src/app/auth/profile/page.tsx
@@ -1,8 +1,13 @@
 'use client';
 
 import { useState } from 'react';
+import { useStore } from 'zustand';
 
 import FilterDropdown from '@/components/FilterDropdown';
+import CollectionUnit from '@/components/Modal/CollectionModal/CollectionUnit';
+import CreateCollectionModal from '@/components/Modal/CreateCollectionModal';
+import useGetAllCollectionList from '@/hooks/serverStateHooks/useGetCollectionList';
+import modalStore from '@/stores/modalStore';
 
 import Icon from '../../../components/Icon/Icon';
 
@@ -10,10 +15,22 @@ const FILTER_OPTIONS = ['최신순', '가나다순'];
 
 const Profile = () => {
   const [selectedFilter, setSelectedFilter] = useState('최신순');
+  const { openModal, closeModal } = useStore(modalStore);
 
   const handleFilterChange = (newFilter: string) => {
     setSelectedFilter(newFilter);
   };
+
+  const handleCreateCollecion = () => {
+    closeModal();
+
+    openModal({
+      modalSize: 'md',
+      contents: <CreateCollectionModal />,
+    });
+  };
+
+  const { collections } = useGetAllCollectionList();
 
   // const handlePageChange = (page: number) => {
   //   setCurrentPage(page);
@@ -22,7 +39,7 @@ const Profile = () => {
   return (
     <div className='pt-[50px] md:px-[140px]'>
       <div className='flex justify-between items-center pb-[70px]'>
-        <button className='button-m'>
+        <button className='button-m' onClick={handleCreateCollecion}>
           <Icon name='Plus' size='m' className='mr-[10px]' />
           컬렉션 생성
         </button>
@@ -36,6 +53,16 @@ const Profile = () => {
       <div>컬렉션 카드</div>
       <Icon name='Lock' size='l' className='text-white' />
       <Icon name='Unlock' size='l' className='text-white mt-[2.5px]' />
+
+      <div className='grid tablet:grid-cols-4 grid-cols-1 gap-x-[33px] gap-y-[30px] max-h-[555px] tablet:pr-[34px] overflow-y-auto scroll-hide'>
+        {collections.map((collection) => (
+          <CollectionUnit
+            key={collection.collectionId}
+            collectionInfo={collection}
+          />
+        ))}
+      </div>
+
       {/* <Pagination
         currentPage={artworkListPage.requestPage}
         totalPages={artworkListPage.totalPages}
--- src/hooks/serverStateHooks/usePostCollection.tsx
+++ src/hooks/serverStateHooks/usePostCollection.tsx
@@ -1,14 +1,19 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { usePathname } from 'next/navigation';
 import { useStore } from 'zustand';
 
 import postCollection, {
   PostCollectionProps,
 } from '@/apis/collection/postCollection';
 import CollectionModal from '@/components/Modal/CollectionModal';
+import CreateCollectionModal from '@/components/Modal/CreateCollectionModal';
+import { COLLECTION } from '@/constants/API';
 import modalStore from '@/stores/modalStore';
 
 const usePostCollection = () => {
   const { openModal, closeModal } = useStore(modalStore);
+  const queryClient = useQueryClient();
+  const isProfile = usePathname().includes('profile');
 
   const { mutate } = useMutation({
     mutationFn: ({ name, isPrivate }: PostCollectionProps) =>
@@ -18,8 +23,14 @@ const usePostCollection = () => {
 
       openModal({
         modalSize: 'lg',
-        contents: <CollectionModal />,
+        contents: isProfile ? <CreateCollectionModal /> : <CollectionModal />,
       });
+
+      if (isProfile) {
+        queryClient.invalidateQueries({
+          queryKey: [COLLECTION.collectionList],
+        });
+      }
     },
   });
 
