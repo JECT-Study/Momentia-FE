29023ea2152416d14293198e6717bfbada75b12a
Dahyeon Jin <wlsekgus12@gmail.com>
2025-02-10 03:31:39 +0900

    feat: collection 타입 추가

--- src/types/collection.ts
+++ src/types/collection.ts
@@ -1,6 +1,14 @@
+import { PaginationType } from './pagination';
+
 export interface CollectionType {
   collectionId: number;
   collectionImage: string;
   name: string;
   status: 'PUBLIC' | 'PRIVATE';
+}
+
+export interface ProfileCollectionListResponse {
+  isMine: boolean;
+  data: CollectionType[];
+  page: PaginationType;
 }
