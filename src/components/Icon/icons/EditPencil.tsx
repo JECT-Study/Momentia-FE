9084f9b42e6b4fd7f62d1161934318f4ef60453a
Dahyeon Jin <wlsekgus12@gmail.com>
2025-02-10 13:45:25 +0900

    style: EditPencil 아이콘 색상 수정

--- src/components/Icon/icons/EditPencil.tsx
+++ src/components/Icon/icons/EditPencil.tsx
@@ -10,11 +10,11 @@ const EditPencil = ({ className }: IconProps) => {
     >
       <path
         d='M15 26.2031H25.5M18.2812 7.17187L22.875 11.1094M6.46875 18.9844L19.4093 5.59187C20.8026 4.19854 23.0617 4.19854 24.455 5.59187C25.8483 6.9852 25.8483 9.24423 24.455 10.6376L11.0625 23.5781L4.5 25.5469L6.46875 18.9844Z'
-        stroke='black'
+        stroke='currentColor'
         strokeWidth='2.1875'
         strokeLinecap='round'
         strokeLinejoin='round'
-        fill='currentColor'
+        fill='none'
       />
     </svg>
   );
