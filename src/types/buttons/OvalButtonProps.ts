dde41d4840d5e68a06c88769eb882b75e9d17cd8
Parksangwoo <psu8430@gmail.com>
2025-02-11 04:13:10 +0900

    feat: Oval Button 컴포넌트 Props 수정 및 disabled 관련 스타일 수정

--- src/types/buttons/OvalButtonProps.ts
+++ src/types/buttons/OvalButtonProps.ts
@@ -1,7 +1,7 @@
 import { BaseButtonProps } from './BaseButtonProps';
 
 export interface OvalButtonProps
-  extends Omit<BaseButtonProps, 'loading' | 'icon' | 'iconPosition' | 'type'> {
+  extends Omit<BaseButtonProps, 'loading' | 'icon' | 'iconPosition'> {
   buttonSize: 's' | 'm';
   className?: string;
 }
