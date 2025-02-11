3c5f44eeb1085717c35ab15432d5ecfe2afaa891
Dahyeon Jin <wlsekgus12@gmail.com>
2025-02-11 19:20:35 +0900

    feat: 컬렉션 내 작품 삭제 API 및 Hook 추가

---
+++ src/apis/collection/deleteCollectionArtwork.ts
@@ -0,0 +1,45 @@
+import { isAxiosError } from 'axios';
+
+import { COLLECTION } from '@/constants/API';
+import {
+  ARTWORK_ERROR_MESSAGE,
+  COMMON_ERROR_MESSAGE,
+} from '@/constants/errorMessage';
+import { CollectionAddAndRemoveArtworkParams } from '@/types/collection';
+import { ErrorResponseType } from '@/types/errorResponse';
+
+import { authorizedClient } from '..';
+
+const deleteCollectionArtwork = async ({
+  collectionId,
+  postId,
+}: CollectionAddAndRemoveArtworkParams) => {
+  try {
+    const response = await authorizedClient.delete<null>(
+      COLLECTION.collectionAddAndRemoveArtwork(collectionId, postId),
+    );
+
+    if (response.status === 204) {
+      return true;
+    } else {
+      throw new Error('작품 삭제 실패');
+    }
+  } catch (error) {
+    if (isAxiosError<ErrorResponseType<null>>(error) && error.response) {
+      const { code } = error;
+
+      if (code) {
+        console.error(ARTWORK_ERROR_MESSAGE[code]);
+        throw new Error(ARTWORK_ERROR_MESSAGE[code]);
+      } else {
+        console.error(COMMON_ERROR_MESSAGE.UNKNOWN_ERROR);
+        throw new Error(COMMON_ERROR_MESSAGE.UNKNOWN_ERROR);
+      }
+    } else {
+      console.error(COMMON_ERROR_MESSAGE.NETWORK_ERROR);
+      throw new Error(COMMON_ERROR_MESSAGE.NETWORK_ERROR);
+    }
+  }
+};
+
+export default deleteCollectionArtwork;
