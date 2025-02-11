c4a9603db7fdced6ff35417bbe89a91f4b17b472
Dahyeon Jin <wlsekgus12@gmail.com>
2025-02-11 16:24:32 +0900

    feat: 컬렉션 내 작품 조회 API 및 Hook 추가

---
+++ src/hooks/serverStateHooks/useGetCollectionArtworks.ts
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+
+import getCollectionArtworks from '@/apis/collection/getCollectionArtworks';
+import { COLLECTION } from '@/constants/API';
+import { PaginationType } from '@/types';
+import { CollectionArtworksParams } from '@/types/collection';
+
+const useGetCollectionArtworks = (params: CollectionArtworksParams) => {
+  const { data, isLoading } = useQuery({
+    queryKey: [COLLECTION.collection, params],
+    queryFn: () => getCollectionArtworks({ ...params }),
+  });
+
+  const response = data || {
+    isMine: false,
+    data: [],
+    page: {} as PaginationType,
+  };
+
+  return {
+    isMine: response.isMine,
+    artworks: response.data,
+    pageInfo: response.page,
+    isLoading,
+  };
+};
+
+export default useGetCollectionArtworks;
