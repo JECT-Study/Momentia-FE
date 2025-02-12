b68892271b9cb9371bff53d2100ef3bcc6a14359
Parksangwoo <psu8430@gmail.com>
2025-02-12 22:51:27 +0900

    chore: 변수 네이밍 변경

--- src/components/ProfilePage/UserInfoSection/index.tsx
+++ src/components/ProfilePage/UserInfoSection/index.tsx
@@ -8,16 +8,16 @@ import DefaultUserInfo from './DefaultUserInfo';
 import EditUserInfo from './EditUserInfo';
 
 const UserInfoSection = ({ userInfo }: { userInfo: UserType }) => {
-  const [isEdit, setIsEdit] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-  const changeMode = () => setIsEdit((state) => !state);
+  const changeMode = () => setIsEditing((state) => !state);
 
   const userNameText = userInfo.isMine ? '나' : userInfo.nickname;
 
   return (
     <div>
       <h2 className='mb-[70px] mobile:mb-[50px]'>{userNameText}의 모멘티아</h2>
-      {isEdit ? (
+      {isEditing ? (
         <EditUserInfo changeMode={changeMode} {...userInfo} />
       ) : (
         <DefaultUserInfo changeMode={changeMode} {...userInfo} />
