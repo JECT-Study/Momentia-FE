295f8f636311a60d3eb33ff4dbe1662ae3ed58fd
Parksangwoo <psu8430@gmail.com>
2025-02-15 00:50:22 +0900

    style: 유저 프로필 사진 영역에 이미지가 꽉차는 스타일로 통일

--- src/components/ArtworkDetailPage/ArtistInfoSection.tsx
+++ src/components/ArtworkDetailPage/ArtistInfoSection.tsx
@@ -32,7 +32,7 @@ const ArtistInfoSection = ({
           <Image
             src={profileImage || '/images/defaultProfileImage.png'}
             alt='profile-image'
-            className='object-contain rounded-full aspect-square'
+            className='object-cover rounded-full aspect-square'
             width={141}
             height={141}
           />
