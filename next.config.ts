60000d3335bd92c0a2ad7b2082a472dbff78ef69
Parksangwoo <psu8430@gmail.com>
2025-02-20 01:30:35 +0900

    settings: docker에서 next config를 활용하기 위한 파일 확장자 수정

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['momentia-dev.kr.object.ncloudstorage.com'],
-  },
-};
-
-export default nextConfig;
