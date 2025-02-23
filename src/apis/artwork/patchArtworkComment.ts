5d662dc9b48d03234408415ea129c75778b54e35
Dahyeon Jin <wlsekgus12@gmail.com>
2025-02-23 22:42:41 +0900

    rename: patchArtworkComment 파일명 오타 정정

--- src/apis/artwork/patchArtworkComment.ts
+++ src/apis/artwork/patchArtworkComment.ts
@@ -16,24 +16,18 @@ const patchArtworkComment = async (commentId: number, content: string) => {
       { content },
     );
 
-    if (response.status === 204) {
-      return content;
-    } else {
-      throw new Error('댓글 수정 실패');
-    }
+    if (response.status === 204) return content;
+    throw new Error('댓글 수정에 실패하였습니다.');
   } catch (error) {
     if (isAxiosError<ErrorResponseType<null>>(error) && error.response) {
       const { code } = error;
 
       if (code) {
-        console.error(COMMENT_ERROR_MESSAGE[code]);
         throw new Error(COMMENT_ERROR_MESSAGE[code]);
       } else {
-        console.error(COMMON_ERROR_MESSAGE.UNKNOWN_ERROR);
         throw new Error(COMMON_ERROR_MESSAGE.UNKNOWN_ERROR);
       }
     } else {
-      console.error(COMMON_ERROR_MESSAGE.NETWORK_ERROR);
       throw new Error(COMMON_ERROR_MESSAGE.NETWORK_ERROR);
     }
   }
