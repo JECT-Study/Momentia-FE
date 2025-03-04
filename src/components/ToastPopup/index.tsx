25f4694f5e416d8dcb27bc8b0dda4fc34d3bea46
Dahyeon Jin <wlsekgus12@gmail.com>
2025-03-04 16:44:18 +0900

    style: ToastPopup이 제거될 때의 UI 동작 수정

--- src/components/ToastPopup/index.tsx
+++ src/components/ToastPopup/index.tsx
@@ -6,13 +6,13 @@ import useToastStore from '@/stores/useToastStore';
 
 import Icon from '../Icon/Icon';
 
-const SPACING = 3;
+const SPACING = 48;
 
 const ToastPopup = () => {
   const { toasts } = useToastStore();
 
-  return toasts.length ? (
-    <div className='fixed top-10 left-1/2 transform -translate-x-1/2 z-50 flex flex-col items-center space-y-2'>
+  return (
+    <div className='fixed top-10 left-1/2 transform -translate-x-1/2 z-50 flex flex-col items-center'>
       <AnimatePresence>
         {toasts.map((toast, index) => (
           <motion.div
@@ -22,6 +22,10 @@ const ToastPopup = () => {
             animate={{ opacity: 1, y: index * SPACING }}
             exit={{ opacity: 0, y: 10 + index * SPACING }}
             transition={{ type: 'spring', stiffness: 300, damping: 20 }}
+            style={{
+              position: 'absolute',
+              top: index === 0 ? 0 : index * SPACING,
+            }}
             className='button-m text-white bg-background-overlay
             w-[366px] mobile:w-[698px] tablet:w-[916px] p-[30px] rounded-[10px]
             shadow-md flex items-center'
@@ -44,7 +48,7 @@ const ToastPopup = () => {
         ))}
       </AnimatePresence>
     </div>
-  ) : null;
+  );
 };
 
 export default ToastPopup;
