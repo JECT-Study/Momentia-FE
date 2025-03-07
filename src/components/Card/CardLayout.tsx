745ef85bed6ee5cffaf9424e2357516c61ecdf1c
Parksangwoo <psu8430@gmail.com>
2025-03-07 17:44:33 +0900

    style: Card Layout 내 너비 스타일 코드 삭제

--- src/components/Card/CardLayout.tsx
+++ src/components/Card/CardLayout.tsx
@@ -9,7 +9,7 @@ interface CardLayoutProps {
 const CardLayout = ({ children, classname, onClick }: CardLayoutProps) => {
   return (
     <div
-      className={`relative overflow-hidden group rounded-[5px] w-full aspect-[4/5] bg-gray-900 cursor-pointer ${classname}`}
+      className={`relative overflow-hidden group rounded-[5px] aspect-[4/5] bg-gray-900 cursor-pointer ${classname}`}
       onClick={onClick}
     >
       {children}
