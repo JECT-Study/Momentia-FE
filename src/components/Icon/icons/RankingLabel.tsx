b6e39ce66195e2ca3def14d02407ea9b0d79da06
Parksangwoo <psu8430@gmail.com>
2025-03-09 00:58:19 +0900

    chore: svg 내부 속성 이름 수정

--- src/components/Icon/icons/RankingLabel.tsx
+++ src/components/Icon/icons/RankingLabel.tsx
@@ -10,16 +10,16 @@ const RankingLabel = ({ className }: IconProps) => {
     >
       <path
         data-figma-bg-blur-radius='8'
-        fill-rule='evenodd'
-        clip-rule='evenodd'
+        fillRule='evenodd'
+        clipRule='evenodd'
         d='M6 0C2.68629 0 0 2.68629 0 6V88.0808C0 92.7526 5.10369 95.6321 9.1025 93.2164L27.8975 81.8621C29.8053 80.7095 32.1947 80.7095 34.1025 81.8621L52.8975 93.2164C56.8963 95.6321 62 92.7526 62 88.0808V6C62 2.68629 59.3137 0 56 0H41.4702H20.5298H6Z'
         fill='#1B1B1B'
       />
       <defs>
         <clipPath id='bgblur_0_2927_12521_clip_path' transform='translate(8 8)'>
           <path
-            fill-rule='evenodd'
-            clip-rule='evenodd'
+            fillRule='evenodd'
+            clipRule='evenodd'
             d='M6 0C2.68629 0 0 2.68629 0 6V88.0808C0 92.7526 5.10369 95.6321 9.1025 93.2164L27.8975 81.8621C29.8053 80.7095 32.1947 80.7095 34.1025 81.8621L52.8975 93.2164C56.8963 95.6321 62 92.7526 62 88.0808V6C62 2.68629 59.3137 0 56 0H41.4702H20.5298H6Z'
           />
         </clipPath>
