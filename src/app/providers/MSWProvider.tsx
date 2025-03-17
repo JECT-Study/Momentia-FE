dd2c04449e3e650c91ddc604cc2885ce304dd2a8
Parksangwoo <psu8430@gmail.com>
2025-03-17 18:02:33 +0900

    fix: prefetching 불가능 이슈로 인한 MSW Provider 내부 로직 롤백

--- src/app/providers/MSWProvider.tsx
+++ src/app/providers/MSWProvider.tsx
@@ -2,27 +2,30 @@
 
 import { ReactNode, useEffect, useState } from 'react';
 
+import Loading from '@/components/Loading';
+
 const MSWProvider = ({ children }: { children: ReactNode }) => {
-  const [isMSWReady, setIsMSWReady] = useState(
-    process.env.NEXT_PUBLIC_USE_MSW !== 'true',
-  );
+  const [isMSWReady, setIsMSWReady] = useState(false);
 
   useEffect(() => {
     const initMsw = async () => {
-      if (isMSWReady) return;
-
       if (process.env.NODE_ENV === 'development') {
         const { initMsw } = await import('@/mocks');
         await initMsw();
       }
-
       setIsMSWReady(true);
     };
 
     initMsw();
   }, []);
 
-  if (!isMSWReady) return null;
+  if (!isMSWReady) {
+    return (
+      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+        <Loading width={300} height={400} />
+      </div>
+    );
+  }
 
   return <>{children}</>;
 };
